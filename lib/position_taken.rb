e5ba7cf49fc11046c268fa05a48b843ea9c3c7ad
Christina Ng <christinasy317@gmail.com>
2018-07-13 15:28:01 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
-def position_taken(board, index)
+def position_taken?(board, index)
   if (board[index] == " " || board[index] == "" || board[index] == nil)
-    false 
-  end 
+    false
+  end
 end
